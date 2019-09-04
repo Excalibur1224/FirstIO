a7da3801dcd5efd2158c90c638450656cea89b6d
Excalibur1224 <bafalla@mail.csuchico.edu>
2019-09-04 16:13:45 -0700

    Changes in the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
+
+//Author: Brayden Falla 
+
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Brayden Falla, What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
